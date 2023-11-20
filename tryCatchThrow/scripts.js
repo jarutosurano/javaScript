7ebeeb5b07fbc147345607627a49ac0976cd83fe
Jayr Sullano <jarutosurano@gmail.com>
2023-11-19 21:01:20 -0500

    try.catch.throw.

---
+++ tryCatchThrow/scripts.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  const message = document.getElementById("pawn");
+  message.innerHTML = "";
+  let x = document.getElementById("inputText");
+  try {
+    if (x.trim() == "") throw "empty";
+    if (isNaN(x)) throw "not a number";
+    x = Number(x);
+    if (x < 5) throw "too low";
+    if (x > 10) throw "too high";
+  } catch (error) {
+    message.innerHTML = `Input is ${error}`;
+  }
+}
